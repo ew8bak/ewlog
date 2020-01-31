8db4f59c298f0b9a2238bafe341dc9802dd6d986
Vladimir Karpenko <i@vladimir.cc>
2020-01-31 17:47:15 +0300

    Добавил отправку на HamQTH Пофиксил отправку на eqslcc, hrdlog Пофиксил приём eqslcc

--- hiddentsettings_u.pas
+++ hiddentsettings_u.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls,
-  httpsend, fpjson, laz_synapse;
+  httpsend;
 
 type
 
@@ -24,51 +24,85 @@ type
     Memo1: TMemo;
     procedure Button1Click(Sender: TObject);
     procedure CatTimerTimer(Sender: TObject);
+    procedure CheckBox1Change(Sender: TObject);
+    procedure CheckBox2Change(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure FormShow(Sender: TObject);
   private
 
   public
+    procedure SendQSO(key, address, call, date, time, freq, mode,
+      submode, rsts, rstr, omname, qth, state, grid, comment: string);
+  public
+    address_serv, API_key: string;
+    apicat, apisend: boolean;
 
   end;
 
 var
   hiddenSettings: ThiddenSettings;
-  address_serv, API_key: string;
-  apicat, apisend: boolean;
+
 
 function SendRadio(freq, mode, dt, key, radio, address: string): string;
-function SendQSO(key, adi, address: string): string;
 
 implementation
 
 uses
-  MainForm_U;
+  MainForm_U, dmFunc_U;
 
 {$R *.lfm}
 
-function SendQSO(key, adi, address: string): string;
+procedure ThiddenSettings.SendQSO(key, address, call, date, time,
+  freq, mode, submode, rsts, rstr, omname, qth, state, grid, comment: string);
 var
   HTTP: THTTPSend;
   temp: TStringStream;
   Response: TStringList;
-begin
-  Result:='';
-  HTTP := THTTPSend.Create;
-  temp := TStringStream.Create('');
-  Response := TStringList.Create;
-  HTTP.MimeType := 'application/json';
-  temp.Size := 0;
-  temp.WriteString('{"key":"' + key + '", "type":"adif", "string":"'+adi+'"}');
-  HTTP.Document.LoadFromStream(temp);
-  if HTTP.HTTPMethod('POST', address) then
+  logdata, json: string;
+
+  procedure AddData(const datatype, Data: string);
   begin
-    Response.LoadFromStream(HTTP.Document);
-    Result := Response.Text;
+    if Data <> '' then
+      logdata := logdata + Format('<%s:%d>%s', [datatype, Length(Data), Data]);
   end;
-  temp.Free;
-  HTTP.Free;
-  Response.Free;
+
+begin
+  try
+    HTTP := THTTPSend.Create;
+    HTTP.MimeType := 'application/json';
+    temp := TStringStream.Create('');
+    Response := TStringList.Create;
+    logdata := '';
+    AddData('CALL', call);
+    AddData('QSO_DATE', date);
+    AddData('TIME_ON', time);
+    AddData('BAND', dmFunc.GetBandFromFreq(freq));
+    AddData('MODE', mode);
+    AddData('SUBMODE', submode);
+    AddData('RST_SENT', rsts);
+    AddData('RST_RCVD', rstr);
+    AddData('COMMENT', comment);
+    AddData('QTH', qth);
+    AddData('NAME', omname);
+    AddData('STATE', state);
+    AddData('GRIDSQUARE', grid);
+    Delete(freq, length(freq) - 2, 1); //Удаляем последнюю точку
+    AddData('FREQ', freq);
+    logdata := logdata + '<EOR>';
+    json := '{"key":"' + key + '", "type":"adif", "string":"' + logdata + '"}';
+    temp.Size := 0;
+    temp.WriteString(json);
+    HTTP.Document.LoadFromStream(temp);
+    if HTTP.HTTPMethod('POST', address) then
+    begin
+      Response.LoadFromStream(HTTP.Document);
+      Memo1.Lines.Add(Response.Text);
+    end;
+  finally
+    temp.Free;
+    HTTP.Free;
+    Response.Free;
+  end;
 end;
 
 function SendRadio(freq, mode, dt, key, radio, address: string): string;
@@ -77,23 +111,26 @@ var
   temp: TStringStream;
   Response: TStringList;
 begin
-  Result:='';
-  HTTP := THTTPSend.Create;
-  temp := TStringStream.Create('');
-  Response := TStringList.Create;
-  HTTP.MimeType := 'application/json';
-  temp.Size := 0;
-  temp.WriteString('{"key":"' + key + '", "radio":"' + radio +
-    '","frequency":' + freq + ',"mode":"' + mode + '","timestamp":"' + dt + '"}');
-  HTTP.Document.LoadFromStream(temp);
-  if HTTP.HTTPMethod('POST', address) then
-  begin
-    Response.LoadFromStream(HTTP.Document);
-    Result := Response.Text;
+  try
+    Result := '';
+    HTTP := THTTPSend.Create;
+    temp := TStringStream.Create('');
+    Response := TStringList.Create;
+    HTTP.MimeType := 'application/json';
+    temp.Size := 0;
+    temp.WriteString('{"key":"' + key + '", "radio":"' + radio +
+      '","frequency":' + freq + ',"mode":"' + mode + '","timestamp":"' + dt + '"}');
+    HTTP.Document.LoadFromStream(temp);
+    if HTTP.HTTPMethod('POST', address) then
+    begin
+      Response.LoadFromStream(HTTP.Document);
+      Result := Response.Text;
+    end;
+  finally
+    temp.Free;
+    HTTP.Free;
+    Response.Free;
   end;
-  temp.Free;
-  HTTP.Free;
-  Response.Free;
 end;
 
 { ThiddenSettings }
@@ -138,9 +175,32 @@ end;
 procedure ThiddenSettings.CatTimerTimer(Sender: TObject);
 begin
   if MainForm.freqchange = True then
-    Memo1.Lines.Add(SendRadio(StringReplace(MainForm.ComboBox1.Text, '.', '', [rfReplaceAll]) +
-      '0', MainForm.ComboBox2.Text, DateTimeToStr(Now), API_key, 'EWLog', address_serv+'/index.php/api/radio/'));
+    Memo1.Lines.Add(SendRadio(StringReplace(MainForm.ComboBox1.Text,
+      '.', '', [rfReplaceAll]) + '0', MainForm.ComboBox2.Text,
+      DateTimeToStr(Now), API_key, 'EWLog', address_serv + '/index.php/api/radio/'));
   MainForm.freqchange := False;
+end;
+
+procedure ThiddenSettings.CheckBox1Change(Sender: TObject);
+begin
+  if CheckBox1.Checked = True then
+  begin
+    apicat := True;
+    CatTimer.Enabled := True;
+  end
+  else
+  begin
+    apicat := False;
+    CatTimer.Enabled := False;
+  end;
+end;
+
+procedure ThiddenSettings.CheckBox2Change(Sender: TObject);
+begin
+  if CheckBox2.Checked = True then
+    apisend := True
+  else
+    apisend := False;
 end;
 
 end.
