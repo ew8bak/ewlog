748cc140fe684926a943d5b28cb46cbc7fdba4bf
vladimir <i@vladimir.cc>
2020-03-13 12:28:05 +0300

    изменено:      mainform_u.pas

--- mainform_u.pas
+++ mainform_u.pas
@@ -3671,7 +3671,7 @@ end;
 procedure TMainForm.LTCPComponent1Error(const msg: string; aSocket: TLSocket);
 begin
  MainForm.StatusBar1.Panels.Items[0].Text := asocket.peerAddress + ':' + SysToUTF8(msg);
-  if Pos('Address already in use', msg) > 0 then begin
+  if (Pos('Address already in use', msg) > 0) or (Pos('Error on bind', msg) > 0) then begin
   LTCPComponent1.Listen(port_tcp[1]);
   lastTCPport:=port_tcp[1];
   MainForm.StatusBar1.Panels.Items[0].Text := '' ;
