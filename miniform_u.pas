e761e6525d4f426a8a61636c49de8179211f88e9
Vladimir <i@vladimir.cc>
2020-09-09 19:03:17 +0300

    miniform

--- miniform_u.pas
+++ miniform_u.pas
@@ -6,7 +6,8 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, StdCtrls,
-  EditBtn, Buttons, ComCtrls, DateTimePicker, LazSysUtils;
+  EditBtn, Buttons, ComCtrls, DateTimePicker, LazSysUtils, foundQSO_record,
+  prefix_record, LCLType;
 
 type
 
@@ -112,6 +113,7 @@ type
     SBState1: TSpeedButton;
     SBCopy: TSpeedButton;
     SBInfo: TSpeedButton;
+    Shape1: TShape;
     TMTime: TTimer;
     TTLocalTimeLabel: TLabel;
     TTUTCLabel: TLabel;
@@ -120,9 +122,14 @@ type
     UTCLabel: TLabel;
     LocalTimeLabel: TLabel;
     Panel1: TPanel;
+    procedure EditCallsignButtonClick(Sender: TObject);
+    procedure EditCallsignChange(Sender: TObject);
+    procedure EditCallsignKeyDown(Sender: TObject; var Key: word;
+      Shift: TShiftState);
     procedure FormShow(Sender: TObject);
     procedure TMTimeTimer(Sender: TObject);
   private
+    SelEditNumChar: integer;
 
   public
 
@@ -130,11 +137,11 @@ type
 
 var
   MiniForm: TMiniForm;
-  TimeDIF: Integer;
+  TimeDIF: integer;
 
 implementation
 
-uses MainFuncDM;
+uses MainFuncDM, InitDB_dm, dmFunc_U, infoDM_U, Earth_Form_U;
 
 {$R *.lfm}
 
@@ -143,6 +150,184 @@ uses MainFuncDM;
 procedure TMiniForm.FormShow(Sender: TObject);
 begin
   IniSet.CurrentForm := 'MINI';
+end;
+
+procedure TMiniForm.EditCallsignButtonClick(Sender: TObject);
+var
+  FoundQSOR: TFoundQSOR;
+begin
+  if InitDB.ImbeddedCallBookConnection.Connected then
+  begin
+    FoundQSOR := MainFunc.FindInCallBook(dmfunc.ExtractCallsign(EditCallsign.Text));
+    EditName.Text := FoundQSOR.OMName;
+    EditQTH.Text := FoundQSOR.OMQTH;
+    EditGrid.Text := FoundQSOR.Grid;
+    EditState.Text := FoundQSOR.State;
+    EditMGR.Text := FoundQSOR.QSLManager;
+  end
+  else
+    InfoDM.GetInformation(dmFunc.ExtractCallsign(EditCallsign.Text), 'MainForm');
+  if not FoundQSOR.Found then
+    InfoDM.GetInformation(dmFunc.ExtractCallsign(EditCallsign.Text), 'MainForm');
+end;
+
+procedure TMiniForm.EditCallsignChange(Sender: TObject);
+var
+  engText: string;
+  DBand, DMode, DCall: boolean;
+  QSL: integer;
+  Lat, Lon: string;
+  PFXR: TPFXR;
+  FoundQSOR: TFoundQSOR;
+  editButtonLeng: integer;
+  editButtonText: string;
+begin
+  DBand := False;
+  DMode := False;
+  DCall := False;
+  LBQSL.Visible := False;
+  LBWorked.Visible := False;
+  LBCfm.Visible := False;
+  QSL := 0;
+  editButtonLeng := Length(EditCallsign.Text);
+  EditCallsign.SelStart := SelEditNumChar;
+  engText := dmFunc.RusToEng(EditCallsign.Text);
+  if (engText <> EditCallsign.Text) then
+  begin
+    EditCallsign.Text := engText;
+    exit;
+  end;
+  editButtonText := EditCallsign.Text;
+
+//  if EditFlag then
+//    Exit;
+
+  if CBFilter.Checked then
+  begin
+    MainFunc.FilterQSO('Call', editButtonText + '%');
+    Exit;
+  end;
+
+  if editButtonText = '' then
+  begin
+    //Clr;
+
+    LBAzimuthD.Caption := '.......';
+    LBDistanceD.Caption := '.......';
+    LBLatitudeD.Caption := '.......';
+    LBLongitudeD.Caption := '.......';
+    LBTerritoryD.Caption := '.......';
+    LBCont.Caption := '.......';
+    LBDXCCD.Caption := '.......';
+    LBPrefixD.Caption := '..';
+    LBCQD.Caption := '..';
+    LBITUD.Caption := '.......';
+    Earth.PaintLine(FloatToStr(LBRecord.OpLat), FloatToStr(LBRecord.OpLon),
+      LBRecord.OpLat, LBRecord.OpLon);
+    Earth.PaintLine(FloatToStr(LBRecord.OpLat), FloatToStr(LBRecord.OpLon),
+      LBRecord.OpLat, LBRecord.OpLon);
+    Exit;
+  end;
+
+  if editButtonLeng > 1 then
+  begin
+    MainFunc.CheckDXCC(editButtonText, CBMode.Text, CBBand.Text,
+      DMode, DBand, DCall);
+    QSL := MainFunc.CheckQSL(editButtonText, CBBand.Text, CBMode.Text);
+    LBWorked.Visible := MainFunc.FindWorkedCall(editButtonText,
+      CBBand.Text, CBMode.Text);
+    LBQSL.Visible := MainFunc.WorkedQSL(editButtonText, CBBand.Text,
+      CBMode.Text);
+    LBCfm.Visible := MainFunc.WorkedLoTW(editButtonText, CBBand.Text,
+      CBMode.Text);
+  end;
+
+  ImDXCCBand.Visible := DBand;
+  ImDXCCMode.Visible := DMode;
+  ImDXCCcountry.Visible := DCall;
+
+  Shape1.Visible := (QSL <> 0);
+
+  if QSL = 1 then
+    Shape1.Brush.Color := clFuchsia;
+
+  if QSL = 2 then
+    Shape1.Brush.Color := clLime;
+
+  if (Sender = CBBand) or (Sender = CBMode) then
+    Exit;
+
+  EditName.Clear;
+  EditQTH.Clear;
+  EditGrid.Clear;
+  EditState.Clear;
+  EditIOTA.Clear;
+  EditMGR.Clear;
+
+  if (editButtonLeng > 0) and (editButtonLeng < 5) then
+  begin
+    PFXR := MainFunc.SearchPrefix(editButtonText, EditMGR.Text);
+    LBAzimuthD.Caption := PFXR.Azimuth;
+    LBDistanceD.Caption := PFXR.Distance;
+    LBLatitudeD.Caption := PFXR.Latitude;
+    LBLongitudeD.Caption := PFXR.Longitude;
+    LBTerritoryD.Caption := PFXR.Country;
+    LBCont.Caption := PFXR.Continent;
+    LBDXCCD.Caption := PFXR.ARRLPrefix;
+    LBPrefixD.Caption := PFXR.Prefix;
+    LBCQD.Caption := PFXR.CQZone;
+    LBITUD.Caption := PFXR.ITUZone;
+    TimeDIF := PFXR.TimeDiff;
+  end;
+  dmFunc.GetLatLon(PFXR.Latitude, PFXR.Longitude, Lat, Lon);
+  Earth.PaintLine(Lat, Lon, LBRecord.OpLat, LBRecord.OpLon);
+  Earth.PaintLine(Lat, Lon, LBRecord.OpLat, LBRecord.OpLon);
+  //MapView
+  // if PFXR.Found and CBMap.Checked then
+ //   MainFunc.LoadMaps(Lat, Lon, MapView1);
+
+  FoundQSOR := MainFunc.FindQSO(dmfunc.ExtractCallsign(editButtonText));
+  EditName.Text := FoundQSOR.OMName;
+  EditQTH.Text := FoundQSOR.OMQTH;
+  EditGrid.Text := FoundQSOR.Grid;
+  EditState.Text := FoundQSOR.State;
+  EditIOTA.Text := FoundQSOR.IOTA;
+  EditMGR.Text := FoundQSOR.QSLManager;
+  LBCount.Caption := IntToStr(FoundQSOR.CountQSO);
+  LBDateQSO.Caption := FoundQSOR.QSODate;
+  LBTimeQSO.Caption := FoundQSOR.QSOTime;
+  LBBandQSO.Caption := FoundQSOR.QSOBand;
+  LBModeQSO.Caption := FoundQSOR.QSOMode;
+  LBNameQSO.Caption := FoundQSOR.OMName;
+
+  if FoundQSOR.Found then
+    EditCallsign.Color := clMoneyGreen
+  else
+    EditCallsign.Color := clDefault;
+
+  if not FoundQSOR.Found and IniSet.UseIntCallBook then
+  begin
+    FoundQSOR := MainFunc.FindInCallBook(dmfunc.ExtractCallsign(editButtonText));
+    EditName.Text := FoundQSOR.OMName;
+    EditQTH.Text := FoundQSOR.OMQTH;
+    EditGrid.Text := FoundQSOR.Grid;
+    EditState.Text := FoundQSOR.State;
+    EditMGR.Text := FoundQSOR.QSLManager;
+  end;
+end;
+
+procedure TMiniForm.EditCallsignKeyDown(Sender: TObject; var Key: word;
+  Shift: TShiftState);
+begin
+  SelEditNumChar := EditCallsign.SelStart + 1;
+  if (Key = VK_BACK) then
+    SelEditNumChar := EditCallsign.SelStart - 1;
+  if (Key = VK_DELETE) then
+    SelEditNumChar := EditCallsign.SelStart;
+  if (EditCallsign.SelLength <> 0) and (Key = VK_BACK) then
+    SelEditNumChar := EditCallsign.SelStart;
+  if (Key = VK_RETURN) then
+    InfoDM.GetInformation(dmFunc.ExtractCallsign(EditCallsign.Text), 'MainForm');
 end;
 
 procedure TMiniForm.TMTimeTimer(Sender: TObject);
