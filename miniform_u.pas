0b333ac4d5b5afcaf9d0d690390a22d82b970ab7
Vladimir <i@vladimir.cc>
2020-09-09 17:36:25 +0300

    miniform

--- miniform_u.pas
+++ miniform_u.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, StdCtrls,
-  EditBtn, Buttons, DateTimePicker;
+  EditBtn, Buttons, ComCtrls, DateTimePicker, LazSysUtils;
 
 type
 
@@ -14,6 +14,13 @@ type
 
   TMiniForm = class(TForm)
     Bevel1: TBevel;
+    Bevel2: TBevel;
+    Bevel3: TBevel;
+    Bevel4: TBevel;
+    Bevel6: TBevel;
+    Bevel7: TBevel;
+    Bevel8: TBevel;
+    Bevel9: TBevel;
     CBBand: TComboBox;
     CBMode: TComboBox;
     CBSubMode: TComboBox;
@@ -25,8 +32,17 @@ type
     CBMark: TCheckBox;
     CBMap: TCheckBox;
     CBFilter: TCheckBox;
+    CBYourQSL: TComboBox;
+    CBCurrentLog: TComboBox;
     DateEdit1: TDateEdit;
     DateTimePicker1: TDateTimePicker;
+    CBMyGrid: TEdit;
+    CBMyState: TEdit;
+    EditState1: TEdit;
+    EditComment: TEdit;
+    EditState4: TEdit;
+    EditState3: TEdit;
+    EditState2: TEdit;
     EditGrid: TEdit;
     EditState: TEdit;
     EditIOTA: TEdit;
@@ -38,6 +54,38 @@ type
     ImDXCCBand: TImage;
     ImDXCCMode: TImage;
     Label10: TLabel;
+    LBCurrentLog: TLabel;
+    LBMyGrid: TLabel;
+    LBMyState: TLabel;
+    LBCount: TLabel;
+    LBDateQSO: TLabel;
+    LBTimeQSO: TLabel;
+    LBBandQSO: TLabel;
+    LBModeQSO: TLabel;
+    LBNameQSO: TLabel;
+    LBAzimuth: TLabel;
+    LBTerritory: TLabel;
+    LBDXCC: TLabel;
+    LBAzimuthD: TLabel;
+    LBTerritoryD: TLabel;
+    LBDXCCD: TLabel;
+    LBDistance: TLabel;
+    LBPrefix: TLabel;
+    LBDistanceD: TLabel;
+    LBPrefixD: TLabel;
+    LBLatitude: TLabel;
+    LBLatitudeD: TLabel;
+    LBLongitude: TLabel;
+    LBLongitudeD: TLabel;
+    LBCont: TLabel;
+    LBCQ: TLabel;
+    LBCQD: TLabel;
+    LBITU: TLabel;
+    LBITUD: TLabel;
+    LBYourQSL: TLabel;
+    LBCfm: TLabel;
+    LBSubState: TLabel;
+    LBComment: TLabel;
     LBGrid: TLabel;
     LBState: TLabel;
     LBIOTA: TLabel;
@@ -58,6 +106,13 @@ type
     SBState: TSpeedButton;
     SBIOTA: TSpeedButton;
     SBMGR: TSpeedButton;
+    SBState4: TSpeedButton;
+    SBState3: TSpeedButton;
+    SBState2: TSpeedButton;
+    SBState1: TSpeedButton;
+    SBCopy: TSpeedButton;
+    SBInfo: TSpeedButton;
+    TMTime: TTimer;
     TTLocalTimeLabel: TLabel;
     TTUTCLabel: TLabel;
     RemoteTimeLabel: TLabel;
@@ -66,6 +121,7 @@ type
     LocalTimeLabel: TLabel;
     Panel1: TPanel;
     procedure FormShow(Sender: TObject);
+    procedure TMTimeTimer(Sender: TObject);
   private
 
   public
@@ -74,8 +130,10 @@ type
 
 var
   MiniForm: TMiniForm;
+  TimeDIF: Integer;
 
 implementation
+
 uses MainFuncDM;
 
 {$R *.lfm}
@@ -84,8 +142,19 @@ uses MainFuncDM;
 
 procedure TMiniForm.FormShow(Sender: TObject);
 begin
-  IniSet.CurrentForm:='MINI';
+  IniSet.CurrentForm := 'MINI';
+end;
+
+procedure TMiniForm.TMTimeTimer(Sender: TObject);
+begin
+  TTLocalTimeLabel.Caption := FormatDateTime('hh:mm:ss', Now);
+  TTUTCLabel.Caption := FormatDateTime('hh:mm:ss', NowUTC);
+  TTRemoteLabel.Caption := FormatDateTime('hh:mm:ss', NowUTC + TimeDIF / 24);
+  if CBRealTime.Checked then
+  begin
+    DateTimePicker1.Time := NowUTC;
+    DateEdit1.Date := NowUTC;
+  end;
 end;
 
 end.
-
